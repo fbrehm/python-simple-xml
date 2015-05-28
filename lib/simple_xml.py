28afe40a12246c2cf1961c302cebe3e7f6fdfd94
Frank Brehm <frank.brehm@profitbricks.com>
2015-05-28 16:51:08 +0200

    Added lib/simple_xml.py

---
+++ lib/simple_xml.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Frank Brehm
+@contact: frank.brehm@profitbricks.com
+@organization: Profitbricks GmbH
+@copyright: © 2010 - 2015 by Profitbricks GmbH
+@license: LGPL3+
+@summary: a module for a simple xml solution.
+"""
+
+
+# Standard modules
+import xml.etree.ElementTree as ET
+from StringIO import StringIO
+
+__author__ = 'Frank Brehm <frank.brehm@profitbricks.com>'
+__copyright__ = '(C) 2010 - 2015 by profitbricks.com'
+__contact__ = 'frank.brehm@profitbricks.com'
+__version__ = '0.1.0'
+__license__ = 'LGPL3+'
+
+
+
+
+#==============================================================================
+
+if __name__ == "__main__":
+    pass
+
+#==============================================================================
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
